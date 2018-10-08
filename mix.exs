e4d02ce5e3f24957be54ab5b7b98285ef4af5b7d
Isaac Whitfield <iw@whitfin.io>
2018-10-08 14:09:47 -0700

    Bump to v1.0.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LocalCluster.MixProject do
   use Mix.Project
 
-  @version "1.0.1"
+  @version "1.0.2"
   @url_docs "http://hexdocs.pm/local_cluster"
   @url_github "https://github.com/whitfin/local-cluster"
 
