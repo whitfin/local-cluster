6f136e84d8df0e9235d9d046032c412d07ebc2a4
Peter Menhart <peter.menhart@mitel.com>
2020-02-26 10:11:31 -0500

    Added option to specify applications to start on child nodes
    * Default behaviour: start_nodes/3 starts all applications loaded on current node
    * Option `:app_names` accepts an ordered list of applications.
      Very useful for test scenarios where only a subset of apps is desired,
      or where order of applications starting is critical.
      Each application is started via `Application.ensure_all_started/2`,
      i.e. with all dependencies.

--- lib/local_cluster.ex
+++ lib/local_cluster.ex
@@ -42,6 +42,18 @@ defmodule LocalCluster do
   and return the names of the nodes to the user for further use. All child
   nodes are linked to the current process, and so will terminate when the
   parent process does for automatic cleanup.
+
+  Option `:app_names` specifies ordered list of applications to be started
+  on child nodes. This is useful if only a subset of applications is needed.
+  Application dependencies are started automatically.
+  Note that only applications already loaded in current node will be started,
+  other app names are silently ignored.
+  Without this option, all currently loaded applications will be included.
+
+  Option `:files` can be used to load additional files onto the remote nodes.
+  Value is a list of absolute file paths to compile on the cluster.
+  This is necessary if you wish to spawn tasks onto the cluster from inside
+  the test code, as the test code is not loaded into the cluster automatically.
   """
   @spec start_nodes(binary, integer, Keyword.t) :: [ atom ]
   def start_nodes(prefix, amount, options \\ [])
@@ -65,10 +77,16 @@ defmodule LocalCluster do
     rpc.(Logger, :configure, [ level: Logger.level() ])
     rpc.(Mix, :env, [ Mix.env() ])
 
-    for { app_name, _, _ } <- Application.loaded_applications() do
+    # copy all application environment values
+    loaded_apps = for { app_name, _, _ } <- Application.loaded_applications() do
       for { key, val } <- Application.get_all_env(app_name) do
         rpc.(Application, :put_env, [ app_name, key, val ])
       end
+      app_name
+    end
+
+    # start apps in specified order
+    for app_name <- Keyword.get(options, :app_names, loaded_apps), app_name in loaded_apps do
       rpc.(Application, :ensure_all_started, [ app_name ])
     end
 
